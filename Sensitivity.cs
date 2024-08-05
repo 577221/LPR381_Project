6cc88d544cf03b71631828b04c5dcb407276a592
Kenneth-klap <141273094+Kenneth-klap@users.noreply.github.com>
2024-08-05 19:32:45 +0200

    Update Sensitivity.cs

--- Sensitivity.cs
+++ Sensitivity.cs
@@ -8,5 +8,15 @@ namespace LPR381_Project
 {
     internal class Sensitivity
     {
+        public void AddNewActivity(double newCoefficient)
+    {
+        ObjectiveCoefficients.Add(newCoefficient);
+        foreach (var constraint in Constraints)
+        {
+            Console.Write($"Enter coefficient for the new activity in constraint with RHS : ");
+            double newConstraintCoefficient = double.Parse(Console.ReadLine());
+            constraint.Coefficients.Add(newConstraintCoefficient);
+        }
+    }
     }
 }
