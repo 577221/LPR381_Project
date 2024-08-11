df1971f592574f5e7ab5ff7abc00f1d78e3f5e7a
Johané Badenhorst <577401@student.belgiumcampus.ac.za>
2024-08-11 13:22:19 +0200

    Model Class removed

--- SolvingMethods/Primal.cs
+++ SolvingMethods/Primal.cs
@@ -1,103 +1,163 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace LPR381_Project.SolvingMethods
 {
     internal class Primal
     {
-        private Model model;
-        private float[,] tableau;
+        private string problemType;
+        private int[] objFunction;
+        private int[,] constraintsCoefficients;
+        private int[] rhsConstraints;
+        private string[] signRestrictions;
+        private float[,] tablua;
 
-        public Primal(Model model)
+        public string ProblemType { get => problemType; set => problemType = value; }
+        public int[] ObjFunction { get => objFunction; set => objFunction = value; }
+        public int[,] ConstraintsCoefficients { get => constraintsCoefficients; set => constraintsCoefficients = value; }
+        public int[] RhsConstraints { get => rhsConstraints; set => rhsConstraints = value; }
+        public string[] SignRestrictions { get => signRestrictions; set => signRestrictions = value; }
+
+        public Primal(string problemType, int[] objFunction, int[,] constraintsCoefficients, int[] rhsConstraints, string[] signRestrictions)
         {
-            this.model = model;
+            this.problemType = problemType;
+            this.objFunction = objFunction;
+            this.constraintsCoefficients = constraintsCoefficients;
+            this.rhsConstraints = rhsConstraints;
+            this.signRestrictions = signRestrictions;
         }
 
         public void Solve()
         {
-            Prepare();
+            //1. Shape tablua
+            //1.1 Put constraints into optimal shape
+            //1.2 Add helper variables
+            /*
+             * obj obj obj rhs
+             * con con con rhs
+             * con con con rhs
+             * con con con rhs
+             */
+            //2. Test on descision variables
+            //3. Ratio test
+            //4. Math on tablua
+            //5. Repaet 2-4 untill optimal
+            //Tabluafy();
+            //Console.WriteLine(ToString());
 
+            Prepare();
             bool optimal = false;
 
             while (!optimal)
             {
                 int chosen = 0;
-                // Select the largest negative value in the objective row (indicating the entering variable)
-                for (int i = 0; i < tableau.GetLength(1); i++)
+                //performs checks on the obj values to select the largest negative value.
+
+                for (int i = 0; i < tablua.GetLength(1); i++)
                 {
-                    if (tableau[0, i] < tableau[0, chosen])
+                    if (tablua[0, i] < tablua[0, chosen])
                     {
                         chosen = i;
                     }
                 }
 
-                // If there's a negative value in the objective row, continue
-                if (tableau[0, chosen] < 0)
+                //if the value at position 0, i in the tablue is smaler than zero, then calculations wil be performed on it
+                if (tablua[0, chosen] < 0)
                 {
+                    //performs calculations on the tableua
                     int pos = 1;
-                    // Perform the ratio test to determine the exiting variable
-                    for (int i = 1; i < tableau.GetLength(0); i++)
+
+                    for (int i = 1; i < tablua.GetLength(0); i++)
                     {
-                        if (tableau[i, chosen] > 0 &&
-                            (tableau[i, tableau.GetLength(1) - 1] / tableau[i, chosen]) <
-                            (tableau[pos, tableau.GetLength(1) - 1] / tableau[pos, chosen]))
+                        //performs the ratio test on rhs values and selects the row with the lowest non-negative ratio
+                        if (((tablua[chosen, i] < tablua[chosen, pos]) && (tablua[chosen, i] >= 0)))
                         {
                             pos = i;
                         }
                     }
-
-                    // TODO: Pivot and update tableau here
-                    // Update tableau based on pivot element at (pos, chosen)
-                }
-                else
-                {
-                    optimal = true;
                 }
             }
         }
 
-        // Prepares the tableau for the Simplex method
-        private void Prepare()
+        //Creates a NxM matrix that contains the lpr problem
+        private Prepare()
         {
-            int numRows = model.ConstraintsCoefficients.GetLength(0) + 1;
-            int numCols = model.ConstraintsCoefficients.GetLength(1) + model.ConstraintsCoefficients.GetLength(0) + 1;
+            float[,] constraints = new float[constraintsCoefficients.GetLength(0), constraintsCoefficients.GetLength(1) + 2];
 
-            tableau = new float[numRows, numCols];
-
-            // Fill objective function row
-            for (int i = 0; i < model.ObjFunction.Length; i++)
+            for (int i = 0; i < constraintsCoefficients.GetLength(0); i++)
             {
-                tableau[0, i] = model.ProblemType == "max" ? -model.ObjFunction[i] : model.ObjFunction[i];
+                for (int j = 0; j < constraintsCoefficients.GetLength(1); j++)
+                {
+                    constraints[i, j] = constraintsCoefficients[i, j];
+                }
+                constraints[i, constraintsCoefficients.GetLength(1)] = 0;
+                constraints[i, constraintsCoefficients.GetLength(1) + 1] = rhsConstraints[i];
             }
 
-            // Fill constraint rows
-            for (int i = 0; i < model.ConstraintsCoefficients.GetLength(0); i++)
+            for (int i = 0; i < constraints.GetLength(0); i++)
             {
-                for (int j = 0; j < model.ConstraintsCoefficients.GetLength(1); j++)
+                for (int j = 0; j < constraints.GetLength(1); j++)
                 {
-                    tableau[i + 1, j] = model.ConstraintsCoefficients[i, j];
+                    //Console.Write(constraints[i, j]+" ");
                 }
+                // Console.WriteLine();
+            }
 
-                tableau[i + 1, numCols - 1] = model.RhsConstraints[i];
-                tableau[i + 1, model.ObjFunction.Length + i] = 1; // Add slack variables
+            float[] obj = new float[constraints.GetLength(1)];
+
+            for (int i = 0; i < objFunction.GetLength(0); i++)
+            {
+                obj[i] = objFunction[i] * (-1);
+                Console.Write(obj[i] + " ");
+
+            }
+
+            for (int i = objFunction.GetLength(0); i < constraints.GetLength(1) - 1; i++)
+            {
+                obj[i] = 0;
+                //Console.Write(obj[i] + " ");
+            }
+
+            //Console.WriteLine();
+            //Console.WriteLine("-----------");
+
+            tablua = new float[constraints.GetLength(0) + 1, constraints.GetLength(1)];
+
+            for (int i = 0; i < obj.GetLength(0) - 1; i++)
+            {
+                tablua[0, i] = obj[i];
+                Console.Write(tablua[0, i] + " ");
+            }
+
+            for (int i = 1; i < constraints.GetLength(0); i++)
+            {
+                Console.WriteLine();
+
+                for (int j = 1; j < constraints.GetLength(1); j++)
+                {
+                    tablua[i, j - 1] = constraints[i - 1, j - 1];
+                    Console.Write(tablua[i, j - 1] + " ");
+                }
             }
         }
 
-        // Returns a string representation of the tableau
+        //Transforms the tableua into a string that can then be written to console
         public override string ToString()
         {
-            StringBuilder sb = new StringBuilder();
+            string myString = string.Empty;
 
-            for (int i = 0; i < tableau.GetLength(0); i++)
+            for (int i = 0; i < tablua.GetLength(0); i++)
             {
-                for (int j = 0; j < tableau.GetLength(1); j++)
+                for (int j = 0; j < tablua.GetLength(1); j++)
                 {
-                    sb.Append(tableau[i, j] + "\t");
+                    myString += tablua[i, j] + " ";
                 }
-                sb.AppendLine();
+                myString += "\n";
             }
-
-            return sb.ToString();
+            return myString;
         }
     }
 }
